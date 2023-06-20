c7a3cf2eb0c43feb71f4db4a3c21f2ab0c127481
Dayllan Alho <dayllan.alho@sou.inteli.edu.br>
2023-06-20 03:40:25 -0300

    Criando funções do backend e Organizando pastas

--- src/Codigo_Fonte/Backend/CRUD.py
+++ src/Codigo_Fonte/Backend/CRUD.py
@@ -1,80 +1,151 @@
-from flask import Flask, jsonify, request, render_template
-from collections import Counter
+from flask import Flask, jsonify, render_template
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+
 
 app = Flask(__name__)
 
-# Dados iniciais (exemplo)
-financial_product_comments = [
-    {"id": 1, "comment": "Esse produto financeiro é incrível!", "sentiment": "positivo"},
-    {"id": 2, "comment": "Não estou satisfeito com os retornos desse produto.", "sentiment": "negativo"},
-    {"id": 3, "comment": "A taxa de juros desse produto é muito alta.", "sentiment": "negativo"}
-]
+# Carregar os DataFrames
+dados_preprocessados = pd.read_csv('dados_preprocessados.csv')
+remocao_autor = pd.read_csv('remocaoautor.csv')
 
-# Rota principal para verificar se a API está rodando
+#ROTAS
+
+# Rota principal para verificar se a API está conectada
 @app.route('/', methods=['GET'])
 def api_status():
-    return jsonify({'status': 'API está em execução'})
+    return jsonify({'status': 'API conectada'})
 
-# Rota para pegar todos os comentários
-@app.route('/comments', methods=['GET'])
-def get_all_comments():
-    return jsonify(financial_product_comments)
 
-# Rota para pegar um comentário específico pelo ID
-@app.route('/comments/<int:comment_id>', methods=['GET'])
-def get_comment(comment_id):
-    comment = next((c for c in financial_product_comments if c['id'] == comment_id), None)
-    if comment:
-        return jsonify(comment)
-    return jsonify({'message': 'Comentário não encontrado'}), 404
 
-# Rota para análise de sentimento de palavras específicas
-@app.route('/sentiment', methods=['POST'])
-def analyze_sentiment():
-    words = request.json['words']
-    sentiment_scores = []
 
-    for word in words:
-        sentiment_scores.append(get_sentiment_score(word))
 
-    return jsonify(sentiment_scores)
+# Rota para contar a quantidade de comentários no DataFrame dados_preprocessados
+@app.route('/quantidade-comentarios', methods=['GET'])
+def quantidade_comentarios():
+    quantidade = len(dados_preprocessados)
+    return jsonify({'quantidade_comentarios': quantidade})
 
-# Rota para criar um gráfico das palavras mais procuradas nos comentários
-@app.route('/chart', methods=['GET'])
-def create_word_frequency_chart():
-    words = [word.lower() for comment in financial_product_comments for word in comment['comment'].split()]
-    word_counts = Counter(words).most_common(10)
-    top_words = [word[0] for word in word_counts]
-    word_frequencies = [word[1] for word in word_counts]
+# Rota para obter o principal sentimento da base
+import numpy as np
 
-    plt.bar(top_words, word_frequencies)
-    plt.xlabel('Palavras')
-    plt.ylabel('Frequência')
-    plt.title('Palavras Mais Procuradas nos Comentários')
-    plt.xticks(rotation=45)
-    plt.tight_layout()
+# Rota para obter dados relacionando quantidade de comentários e principal sentimento
+@app.route('/dados-relacionados', methods=['GET'])
+def dados_relacionados():
+    sentimentos = dados_preprocessados['sentimento'].value_counts()
+    dados = []
+    for sentimento, quantidade in sentimentos.items():
+        dados.append({'sentimento': sentimento, 'quantidade_comentarios': quantidade})
+    return jsonify({'dados_relacionados': dados})
 
+@app.route('/detalhamento-mensal', methods=['GET'])
+def detalhamento_mensal():
+    mes = request.args.get('mes')  # Parâmetro para alternar entre os meses
+    data_selecionada = dados_preprocessados[dados_preprocessados['data'].dt.month == int(mes)]
+    
+    sentimentos_mensais = data_selecionada['sentimento'].value_counts()
+    quantidade_negativo = sentimentos_mensais.get('NEGATIVO', 0)
+    quantidade_positivo = sentimentos_mensais.get('POSITIVO', 0)
+    quantidade_neutro = sentimentos_mensais.get('NEUTRO', 0)
+    
+    return jsonify({
+        'mes': mes,
+        'quantidade_negativo': quantidade_negativo,
+        'quantidade_positivo': quantidade_positivo,
+        'quantidade_neutro': quantidade_neutro
+    })
+
+@app.route('/analise-sentimento-mensal', methods=['GET'])
+def analise_sentimento_mensal():
+    comentarios_por_mes = dados_preprocessados['data'].dt.to_period('M').value_counts().sort_index()
+    
+    return jsonify(comentarios_por_mes.to_dict())
+
+@app.route('/interacoes', methods=['GET'])
+def interacoes():
+    comentarios_por_mes = dados_preprocessados['data'].dt.to_period('M').value_counts().sort_index()
+    sentimentos = dados_preprocessados['sentimento'].value_counts()
+    
+    return jsonify({
+        'comentarios_por_mes': comentarios_por_mes.to_dict(),
+        'sentimentos': sentimentos.to_dict()
+    })
+
+# Rota para gerar um gráfico de rosca com a quantidade de cada sentimento
+@app.route('/grafico-rosca', methods=['GET'])
+def grafico_rosca():
+    sentimentos = dados_preprocessados['sentimento'].value_counts()
+
+    # Configuração do gráfico
+    plt.figure(figsize=(6, 6))
+    plt.pie(sentimentos.values, labels=sentimentos.index, autopct='%1.1f%%', startangle=90)
+    plt.axis('equal')
+    plt.title('Distribuição de Sentimentos')
+    
     # Salvar o gráfico em um arquivo temporário
-    chart_file = 'word_frequency_chart.png'
+    chart_file = 'grafico_rosca.png'
     plt.savefig(chart_file)
     plt.close()
+    
+    return render_template('grafico.html', chart_file=chart_file)
 
-    return render_template('chart.html', chart_file=chart_file)
+@app.route('/principal-sentimento', methods=['GET'])
+def principal_sentimento():
+    sentimentos = dados_preprocessados['sentimento'].value_counts()
+    principal = sentimentos.idxmax()
+    quantidade = sentimentos[principal]
+    
+    try:
+        principal_int = int(principal)
+    except ValueError:
+        principal_int = None
 
-# Função auxiliar para obter o sentimento de uma palavra
-def get_sentiment_score(word):
-    # Lógica para obter o sentimento da palavra
-    # Substitua este trecho com a sua própria implementação de análise de sentimento
+    # Converter para int se for um valor numpy.int64
+    if isinstance(principal_int, np.int64):
+        principal_int = int(principal_int)
+    
+    return jsonify({'principal_sentimento': principal_int, 'quantidade': quantidade})
 
-    # Exemplo simples: atribuir positivo a palavras com mais de 3 letras, negativo caso contrário e neutro caso seja uma palavra vazia
-    if len(word) > 3:
-        return 'positivo'
-    elif len(word) > 0:
-        return 'negativo'
-    else:
-        return 'neutro'
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-# Iniciar o servidor Flask
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+@app.route('/principal-palavras', methods=['GET'])
+def principal_palavras():
+    stop_words = set(stopwords.words('portuguese'))  # Definindo as stopwords em português
+    palavras_indesejadas = ['não', 'btgpactual', 'btg', 'voce', 'sobre', 'dia', 'pactual','nao',
+                            'banco', 'ja', 'ja', 'sao', 'são', 'link', 'só', 'so', 'melhor','hoje',
+                            'hj', 'todos','pra','tambem','todos','r','maior', 'evento', 'tudo', 'ter',
+                            'ainda', 'fazer', 'ter','bio','investimento', 'voces', 'vocês','aqui','ano',
+                            'time','sempre','obrigado','pode','agora', 'ate','até','semana','investir']
+    textos = dados_preprocessados['texto']
+    
+    # Remoção de stopwords e contagem de palavras
+    palavras = {}
+    for texto in textos:
+        if isinstance(texto, str):  # Verifica se o valor é uma string
+            tokens = word_tokenize(texto.lower())  # Tokenização e conversão para minúsculas
+            palavras_filtradas = [palavra for palavra in tokens if palavra.isalpha() and palavra not in stop_words and palavra not in palavras_indesejadas]
+            
+            for palavra in palavras_filtradas:
+                if palavra in palavras:
+                    palavras[palavra] += 1
+                else:
+                    palavras[palavra] = 1
+    
+    # Ordenando as palavras por frequência
+    palavras_principais = sorted(palavras.items(), key=lambda x: x[1], reverse=True)[:10]  # Selecionando as 10 palavras mais frequentes
+    
+    return jsonify(palavras_principais)
+
+
 if __name__ == '__main__':
     app.run()
