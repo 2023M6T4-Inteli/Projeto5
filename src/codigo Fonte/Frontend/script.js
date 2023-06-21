f893b1a07f849e072caebb9af49041f318a64527
Lucas211-ctrl <lucas.vieira@sou.inteli.edu.br>
2023-06-21 01:47:26 -0300

    Criação Página de Sentimentos

---
+++ src/codigo Fonte/Frontend/script.js
@@ -0,0 +1,41 @@
+// Gráfico de Barras
+
+const dataBarras = {
+    labels: ['Negativo', 'Positivo', 'Neutro'],
+    datasets: [
+      {
+        label:  '',
+        data: [10, 15, 5],
+        backgroundColor: ['#FF3333', '#0DBC53', '#B5B5B5']
+      }
+    ]
+  };
+  
+  const optionsBarras = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        beginAtZero: true
+      },
+      y: {
+        beginAtZero: true
+      }
+    },
+    plugins: {
+      legend: {
+        display: true,
+        labels: {
+          boxWidth: 0 // Definir a largura da caixa de cor como 0 para removê-la
+        }
+      }
+    }
+  };
+  
+  const ctxBarras = document.getElementById('g6Barras').getContext('2d');
+  const myChartBarras = new Chart(ctxBarras, {
+    type: 'bar',
+    data: dataBarras,
+    options: optionsBarras
+  });
+  
