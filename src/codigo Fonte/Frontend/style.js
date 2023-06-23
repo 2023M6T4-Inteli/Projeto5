cfe5d7bdb54eea3bffd8b9bd781e6476dc3329ca
Giovanna <giovanna.torres@sou.inteli.edu.br>
2023-06-23 00:50:53 -0300

    Documentação final de Instalação
    Mudança de alocação de pastas e atualizações front-end.

--- src/codigo Fonte/Frontend/style.js
+++ src/codigo Fonte/Frontend/style.js
@@ -1,5 +1,6 @@
 // Gráfico Rosca
-var posicaoMaior = 0;
+var posicaoMaior;
+var resultadoCategoria;
 
 // Função para criar/atualizar o gráfico de rosca
 function criarGraficoRosca(data) {
@@ -13,10 +14,10 @@ function criarGraficoRosca(data) {
   var myChartRosca = new Chart(ctxRosca, {
     type: 'doughnut',
     data: {
-      labels: ['Negativo', 'Positivo', 'Neutro'],
+      labels: ['Neutro', 'Negativo', 'Positivo'],
       datasets: [{
         data: valoresComentarios, // Atualize os dados com os valores de quantidade_comentarios
-        backgroundColor: ['#FF3333', '#0DBC53', '#B5B5B5']
+        backgroundColor: ['#B5B5B5', '#FF3333',  '#0DBC53']
       }]
     },
     options: {
@@ -42,6 +43,7 @@ function criarGraficoRosca(data) {
   });
 
   var maiorValor = valoresComentarios[0];
+  posicaoMaior = 0;
 
   for (var i = 1; i < valoresComentarios.length; i++) {
     if (valoresComentarios[i] > maiorValor) {
@@ -52,52 +54,60 @@ function criarGraficoRosca(data) {
 
   console.log("A posição do maior valor é:", posicaoMaior);
 
-  categoria(posicaoMaior); // Chamar a função categoria dentro de criarGraficoRosca
+  resultadoCategoria = categoria(posicaoMaior); // Chamar a função categoria e atribuir o resultado a resultadoCategoria
+
+  document.getElementById('principalSentimento').innerHTML = resultadoCategoria;
 
   return posicaoMaior;
 }
 
 function categoria(posicaoMaior) {
   if (posicaoMaior == 0) {
-    return "Negativo";
-  } else if (posicaoMaior == 1) {
-    return "Positivo";
-  } else if (posicaoMaior == 2) {
     return "Neutro";
+  } else if (posicaoMaior == 1) {
+    return "Negativo";
+  } else if (posicaoMaior == 2) {
+    return "Positivo";
+  } else {
+    return "";
   }
 }
 
-// var posicaoMaior = criarGraficoRosca(data);
-var resultadoCategoria = categoria(posicaoMaior);
 console.log(resultadoCategoria);
 
-document.getElementById('principalSentimento').innerHTML = resultadoCategoria;
+
+
 
 // Gráfico Linha
 
+// style.js
 const dataLinha = {
-    labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-    datasets: [
-      {
-        label: 'Positivo',
-        data: [15, 12, 20, 18, 25, 22, 28, 30, 32, 28, 20, 18],
-        borderColor: '#0DBC53',
-        backgroundColor: '#0DBC53'
-      },
-      {
-        label: 'Negativo',
-        data: [8, 10, 6, 12, 9, 15, 11, 13, 10, 14, 9, 7],
-        borderColor: '#FF3333',
-        backgroundColor: '#FF3333'
-      },
-      {
-        label: 'Neutro',
-        data: [5, 3, 4, 6, 8, 7, 4, 6, 3, 5, 4, 6],
-        borderColor: '#B5B5B5',
-        backgroundColor: '#B5B5B5'
-      }
-    ]
-  };
+  labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+  datasets: [
+    {
+      label: 'Positivo',
+      data: [], // Deixe os dados vazios inicialmente
+      borderColor: '#0DBC53',
+      backgroundColor: '#0DBC53'
+    },
+    {
+      label: 'Negativo',
+      data: [], // Deixe os dados vazios inicialmente
+      borderColor: '#FF3333',
+      backgroundColor: '#FF3333'
+    },
+    {
+      label: 'Neutro',
+      data: [], // Deixe os dados vazios inicialmente
+      borderColor: '#B5B5B5',
+      backgroundColor: '#B5B5B5'
+    }
+  ]
+};
+
+// Exporte a constante dataLinha para que possa ser usada em outros arquivos
+console.log(dataLinha.labels); // ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+
   
   const optionsLinha = {
     responsive: true,
@@ -176,116 +186,240 @@ const dataBarras = {
   
 // Gráfico de Dispersão  
 
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   var ctx = document.getElementById('g7Dispersao').getContext('2d');
+  
+  //   var negativo = [
+        
+  //       { x: 2, y: 5 }, 
+  //       { x: 2.5, y: 7 }, 
+  //       { x: 4, y: 8 }, 
+  //       { x: 1, y: 3 }, 
+  //       { x: 5, y: 9 },
+  //       { x: 1, y: 3.09 },
+  //       { x: 12, y: 27.78},
+  //       { x: 7, y: 5.51 },
+  //       { x: 6, y: 9.14 },
+  //       { x: 11, y: 22.47},
+  //       { x: 3, y: 18.07 },
+  //       { x: 5, y: 29.84 },
+  //       { x: 9, y: 8.89 },
+  //       { x: 4, y: 26.31 },
+  //       { x: 8, y: 13.46 },
+    
+  //   ];
+
+  //   var neutro = [
+        
+  //       { x: 6, y: 7 }, 
+  //       { x: 3, y: 2 }, 
+  //       { x: 8, y: 4 },
+  //       { x: 9.4, y: 13.27 },
+  //       { x: 3.2, y: 28.19 },
+  //       { x: 11.7, y: 0.46 },
+  //       { x: 7.1, y: 22.91 },
+  //       { x: 6.4, y: 27.33 },
+  //       { x: 10.5, y: 16.65 },
+  //       { x: 5.9, y: 24.01 },
+  //       { x: 12, y: 10.12 },
+  //       { x: 9.5, y: 12.56 },
+  //       { x: 2.2, y: 21.74 },
+    
+  //   ];
+  //   var positivo = [
+
+  //       { x: 9, y: 1 }, 
+  //       { x: 7, y: 6 }, 
+  //       { x: 10, y: 3 }, 
+  //       { x: 5.3, y: 3 },
+  //       { x: 4, y: 11.89 },
+  //       { x: 7, y: 23.04 },
+  //       { x: 10, y: 15.28 },
+  //       { x: 3, y: 2.34 },
+  //       { x: 8, y: 14.93 },
+  //       { x: 6, y: 7.61 },
+  //       { x: 2, y: 25.57 },
+  //       { x: 11, y: 6.79 },
+  //       { x: 9, y: 19.12 },
+  //       { x: 5, y: 28.76 },
+    
+  //   ];
+  
+  //   var scatterChart = new Chart(ctx, {
+  //     type: 'scatter',
+  //     data: {
+  //       datasets: [
+  //         {
+  //           label: 'Negativo',
+  //           data: negativo,
+  //           backgroundColor: '#FF3333'
+  //         },
+  //         {
+  //           label: 'Neutro',
+  //           data: neutro,
+  //           backgroundColor: '#B5B5B5'
+  //         },
+  //         {
+  //           label: 'Positivo',
+  //           data: positivo,
+  //           backgroundColor: '#0DBC53'
+  //         }
+  //       ]
+  //     },
+  //     options: {
+  //       scales: {
+  //         x: {
+  //           type: 'linear',
+  //           position: 'bottom',
+  //           ticks: {
+  //             stepSize: 1,
+  //             min: 1,
+  //             max: 12
+  //           }
+  //         },
+  //         y: {
+  //           type: 'linear',
+  //           position: 'left'
+  //         }
+  //       },
+  //       plugins: {
+  //         legend: {
+  //           display: true,
+  //           position: 'bottom',
+  //           labels: {
+  //             usePointStyle: true,
+  //             boxWidth: 6,
+  //             font: {
+  //               size: 6
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   });
+  // });
+  
+
+
+ 
   document.addEventListener('DOMContentLoaded', function() {
-    var ctx = document.getElementById('g7Dispersao').getContext('2d');
+    const selectCategory = document.getElementById('selectMesesG7');
+    const ctx = document.getElementById('g7Dispersao').getContext('2d');
+    let myChartDispersao;
   
-    var negativo = [
-        
-        { x: 2, y: 5 }, 
-        { x: 2.5, y: 7 }, 
-        { x: 4, y: 8 }, 
-        { x: 1, y: 3 }, 
-        { x: 5, y: 9 },
-        { x: 1, y: 3.09 },
-        { x: 12, y: 27.78},
-        { x: 7, y: 5.51 },
-        { x: 6, y: 9.14 },
-        { x: 11, y: 22.47},
-        { x: 3, y: 18.07 },
-        { x: 5, y: 29.84 },
-        { x: 9, y: 8.89 },
-        { x: 4, y: 26.31 },
-        { x: 8, y: 13.46 },
-    
-    ];
-
-    var neutro = [
-        
-        { x: 6, y: 7 }, 
-        { x: 3, y: 2 }, 
-        { x: 8, y: 4 },
-        { x: 9.4, y: 13.27 },
-        { x: 3.2, y: 28.19 },
-        { x: 11.7, y: 0.46 },
-        { x: 7.1, y: 22.91 },
-        { x: 6.4, y: 27.33 },
-        { x: 10.5, y: 16.65 },
-        { x: 5.9, y: 24.01 },
-        { x: 12, y: 10.12 },
-        { x: 9.5, y: 12.56 },
-        { x: 2.2, y: 21.74 },
-    
-    ];
-    var positivo = [
-
-        { x: 9, y: 1 }, 
-        { x: 7, y: 6 }, 
-        { x: 10, y: 3 }, 
-        { x: 5.3, y: 3 },
-        { x: 4, y: 11.89 },
-        { x: 7, y: 23.04 },
-        { x: 10, y: 15.28 },
-        { x: 3, y: 2.34 },
-        { x: 8, y: 14.93 },
-        { x: 6, y: 7.61 },
-        { x: 2, y: 25.57 },
-        { x: 11, y: 6.79 },
-        { x: 9, y: 19.12 },
-        { x: 5, y: 28.76 },
-    
-    ];
+    selectCategory.addEventListener('change', () => {
+      const selectedCategory = selectCategory.value;
+      // Aqui você pode adicionar a lógica para atualizar os dados e as opções do gráfico com base na categoria selecionada
+    });
   
-    var scatterChart = new Chart(ctx, {
-      type: 'scatter',
-      data: {
-        datasets: [
-          {
-            label: 'Negativo',
-            data: negativo,
-            backgroundColor: '#FF3333'
-          },
-          {
-            label: 'Neutro',
-            data: neutro,
-            backgroundColor: '#B5B5B5'
-          },
-          {
-            label: 'Positivo',
-            data: positivo,
-            backgroundColor: '#0DBC53'
-          }
-        ]
-      },
-      options: {
-        scales: {
-          x: {
-            type: 'linear',
-            position: 'bottom',
-            ticks: {
-              stepSize: 1,
-              min: 1,
-              max: 12
+    function fetchComentariosDia() {
+      return fetch('http://127.0.0.1:5000/comentarios-dia')
+        .then(response => response.json())
+        .then(data => {
+          return data;
+        })
+        .catch(error => {
+          console.error('Erro ao obter os dados do servidor:', error);
+        });
+    }
+  
+    fetchComentariosDia()
+      .then(data => {
+        console.log(data);
+  
+        // Extrair os dias e a quantidade de comentários para cada dia
+        const dias = Object.keys(data);
+        const quantidadeComentarios = Object.values(data);
+  
+        // Criar os arrays de dados para o gráfico de dispersão
+        const comentarioData = dias.map((dia, index) => ({ x: new Date(dia), y: quantidadeComentarios[index] }));
+        const marcacaoData = [];
+        const respostaData = [];
+  
+        // Configurar os dados do gráfico de dispersão
+        const dataDispersao = {
+          datasets: [
+            {
+              label: 'Comentários',
+              data: comentarioData,
+              backgroundColor: '#0DBC53',
+              borderColor: '#0DBC53',
+              borderWidth: 1,
+              pointRadius: 4,
+              pointHoverRadius: 6,
+              pointHitRadius: 10
+            },
+            {
+              label: 'Marcações',
+              data: marcacaoData,
+              backgroundColor: '#FF3333',
+              borderColor: '#FF3333',
+              borderWidth: 1,
+              pointRadius: 4,
+              pointHoverRadius: 6,
+              pointHitRadius: 10
+            },
+            {
+              label: 'Respostas',
+              data: respostaData,
+              backgroundColor: '#B5B5B5',
+              borderColor: '#B5B5B5',
+              borderWidth: 1,
+              pointRadius: 4,
+              pointHoverRadius: 6,
+              pointHitRadius: 10
+            }
+          ]
+        };
+  
+        // Configurar as opções do gráfico de dispersão
+        const optionsDispersao = {
+          responsive: true,
+          maintainAspectRatio: false,
+          plugins: {
+            tooltip: {
+              callbacks: {
+                label: function(context) {
+                  const datasetLabel = context.dataset.label || '';
+                  const dataPoint = context.parsed;
+                  return datasetLabel + ' - ' + dataPoint.y;
+                }
+              }
             }
           },
-          y: {
-            type: 'linear',
-            position: 'left'
-          }
-        },
-        plugins: {
-          legend: {
-            display: true,
-            position: 'bottom',
-            labels: {
-              usePointStyle: true,
-              boxWidth: 6,
-              font: {
-                size: 6
+          scales: {
+            x: {
+              type: 'time',
+              title: {
+                display: true,
+                text: 'Dia'
+              }
+            },
+            y: {
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Quantidade'
               }
             }
           }
+        };
+  
+        // Destruir o gráfico de dispersão anterior, se existir
+        if (myChartDispersao) {
+          myChartDispersao.destroy();
         }
-      }
-    });
+  
+        // Criar o gráfico de dispersão usando a biblioteca Chart.js
+        myChartDispersao = new Chart(ctx, {
+          type: 'scatter',
+          data: dataDispersao,
+          options: optionsDispersao
+        });
+      })
+      .catch(error => {
+        console.error('Erro ao obter os dados do servidor:', error);
+      });
   });
+  
   
