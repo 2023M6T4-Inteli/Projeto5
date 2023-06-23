cbf784abb26b910a839353aa42a770d6d04af882
Dayllan Alho <dayllan.alho@sou.inteli.edu.br>
2023-06-23 03:31:14 -0300

    Alterações Gerais - M6 (Giovanna, Lucas e Dayllan)

--- src/codigo Fonte/Frontend/style.js
+++ src/codigo Fonte/Frontend/style.js
@@ -184,120 +184,121 @@ const dataBarras = {
     options: optionsBarras
   });
   
+  
 // Gráfico de Dispersão  
 
-  // document.addEventListener('DOMContentLoaded', function() {
-  //   var ctx = document.getElementById('g7Dispersao').getContext('2d');
+  document.addEventListener('DOMContentLoaded', function() {
+    var ctx = document.getElementById('g7Dispersao').getContext('2d');
   
-  //   var negativo = [
+    var negativo = [
         
-  //       { x: 2, y: 5 }, 
-  //       { x: 2.5, y: 7 }, 
-  //       { x: 4, y: 8 }, 
-  //       { x: 1, y: 3 }, 
-  //       { x: 5, y: 9 },
-  //       { x: 1, y: 3.09 },
-  //       { x: 12, y: 27.78},
-  //       { x: 7, y: 5.51 },
-  //       { x: 6, y: 9.14 },
-  //       { x: 11, y: 22.47},
-  //       { x: 3, y: 18.07 },
-  //       { x: 5, y: 29.84 },
-  //       { x: 9, y: 8.89 },
-  //       { x: 4, y: 26.31 },
-  //       { x: 8, y: 13.46 },
+        { x: 2, y: 5 }, 
+        { x: 2.5, y: 7 }, 
+        { x: 4, y: 8 }, 
+        { x: 1, y: 3 }, 
+        { x: 5, y: 9 },
+        { x: 1, y: 3.09 },
+        { x: 12, y: 27.78},
+        { x: 7, y: 5.51 },
+        { x: 6, y: 9.14 },
+        { x: 11, y: 22.47},
+        { x: 3, y: 18.07 },
+        { x: 5, y: 29.84 },
+        { x: 9, y: 8.89 },
+        { x: 4, y: 26.31 },
+        { x: 8, y: 13.46 },
     
-  //   ];
+    ];
 
-  //   var neutro = [
+    var neutro = [
         
-  //       { x: 6, y: 7 }, 
-  //       { x: 3, y: 2 }, 
-  //       { x: 8, y: 4 },
-  //       { x: 9.4, y: 13.27 },
-  //       { x: 3.2, y: 28.19 },
-  //       { x: 11.7, y: 0.46 },
-  //       { x: 7.1, y: 22.91 },
-  //       { x: 6.4, y: 27.33 },
-  //       { x: 10.5, y: 16.65 },
-  //       { x: 5.9, y: 24.01 },
-  //       { x: 12, y: 10.12 },
-  //       { x: 9.5, y: 12.56 },
-  //       { x: 2.2, y: 21.74 },
+        { x: 6, y: 7 }, 
+        { x: 3, y: 2 }, 
+        { x: 8, y: 4 },
+        { x: 9.4, y: 13.27 },
+        { x: 3.2, y: 28.19 },
+        { x: 11.7, y: 0.46 },
+        { x: 7.1, y: 22.91 },
+        { x: 6.4, y: 27.33 },
+        { x: 10.5, y: 16.65 },
+        { x: 5.9, y: 24.01 },
+        { x: 12, y: 10.12 },
+        { x: 9.5, y: 12.56 },
+        { x: 2.2, y: 21.74 },
     
-  //   ];
-  //   var positivo = [
+    ];
+    var positivo = [
 
-  //       { x: 9, y: 1 }, 
-  //       { x: 7, y: 6 }, 
-  //       { x: 10, y: 3 }, 
-  //       { x: 5.3, y: 3 },
-  //       { x: 4, y: 11.89 },
-  //       { x: 7, y: 23.04 },
-  //       { x: 10, y: 15.28 },
-  //       { x: 3, y: 2.34 },
-  //       { x: 8, y: 14.93 },
-  //       { x: 6, y: 7.61 },
-  //       { x: 2, y: 25.57 },
-  //       { x: 11, y: 6.79 },
-  //       { x: 9, y: 19.12 },
-  //       { x: 5, y: 28.76 },
+        { x: 9, y: 1 }, 
+        { x: 7, y: 6 }, 
+        { x: 10, y: 3 }, 
+        { x: 5.3, y: 3 },
+        { x: 4, y: 11.89 },
+        { x: 7, y: 23.04 },
+        { x: 10, y: 15.28 },
+        { x: 3, y: 2.34 },
+        { x: 8, y: 14.93 },
+        { x: 6, y: 7.61 },
+        { x: 2, y: 25.57 },
+        { x: 11, y: 6.79 },
+        { x: 9, y: 19.12 },
+        { x: 5, y: 28.76 },
     
-  //   ];
+    ];
   
-  //   var scatterChart = new Chart(ctx, {
-  //     type: 'scatter',
-  //     data: {
-  //       datasets: [
-  //         {
-  //           label: 'Negativo',
-  //           data: negativo,
-  //           backgroundColor: '#FF3333'
-  //         },
-  //         {
-  //           label: 'Neutro',
-  //           data: neutro,
-  //           backgroundColor: '#B5B5B5'
-  //         },
-  //         {
-  //           label: 'Positivo',
-  //           data: positivo,
-  //           backgroundColor: '#0DBC53'
-  //         }
-  //       ]
-  //     },
-  //     options: {
-  //       scales: {
-  //         x: {
-  //           type: 'linear',
-  //           position: 'bottom',
-  //           ticks: {
-  //             stepSize: 1,
-  //             min: 1,
-  //             max: 12
-  //           }
-  //         },
-  //         y: {
-  //           type: 'linear',
-  //           position: 'left'
-  //         }
-  //       },
-  //       plugins: {
-  //         legend: {
-  //           display: true,
-  //           position: 'bottom',
-  //           labels: {
-  //             usePointStyle: true,
-  //             boxWidth: 6,
-  //             font: {
-  //               size: 6
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   });
-  // });
+    var scatterChart = new Chart(ctx, {
+      type: 'scatter',
+      data: {
+        datasets: [
+          {
+            label: 'Negativo',
+            data: negativo,
+            backgroundColor: '#FF3333'
+          },
+          {
+            label: 'Neutro',
+            data: neutro,
+            backgroundColor: '#B5B5B5'
+          },
+          {
+            label: 'Positivo',
+            data: positivo,
+            backgroundColor: '#0DBC53'
+          }
+        ]
+      },
+      options: {
+        scales: {
+          x: {
+            type: 'linear',
+            position: 'bottom',
+            ticks: {
+              stepSize: 1,
+              min: 1,
+              max: 12
+            }
+          },
+          y: {
+            type: 'linear',
+            position: 'left'
+          }
+        },
+        plugins: {
+          legend: {
+            display: true,
+            position: 'bottom',
+            labels: {
+              usePointStyle: true,
+              boxWidth: 6,
+              font: {
+                size: 6
+              }
+            }
+          }
+        }
+      }
+    });
+  });
   
 
 
